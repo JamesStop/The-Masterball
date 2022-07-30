ed1bff1e91595966b88913cbe6ade4a5cab2e903
xelphite <stoppanij@gmail.com>
2022-07-29 19:25:26 -0700

    pokemon page now gets pokemon information based on url id and stores in state}

--- src/components/PokemonPage/PokemonPage.js
+++ src/components/PokemonPage/PokemonPage.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import './PokemonPage.css'
 
 function PokemonPage(props) {
-    return (
-        <div>
-            PokemonPage
-        </div>
-    );
+	const [pokemon, setPokemon] = useState({});
+	const { id } = useParams();
+
+	useEffect(() => {
+		getPokemon();
+	}, []);
+
+	const getPokemon = async () => {
+		try {
+			const response = await axios.get(
+				`http://localhost:1738/api/pokemon/${id}`
+			);
+			const results = await response.data;
+			setPokemon(results);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return <div>{pokemon.name}</div>;
 }
 
 export default PokemonPage;
